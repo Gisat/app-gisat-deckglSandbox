b7bf6f583aaca69f40216920e4165142fc9b4c9f
Mariana Kecová <mariana.kecova@gisat.cz>
2024-09-09 16:47:01 +0200

    Icon arrows updated
    gui added, mvt added, url data updated, arrows updated

--- src/maps/D8_iconArrows/index.jsx
+++ src/maps/D8_iconArrows/index.jsx
@@ -1,25 +1,20 @@
 // src/maps/MapApp1.jsx
+import { useEffect, useState } from 'react';
 import { DeckGL } from 'deck.gl';
 import { MapView } from '@deck.gl/core';
 import { TileLayer } from '@deck.gl/geo-layers';
-import { BitmapLayer, IconLayer } from '@deck.gl/layers';
+import {BitmapLayer, IconLayer} from '@deck.gl/layers';
+import {scaleLinear} from 'd3-scale';
+import {OBJLoader} from '@loaders.gl/obj';
 import { MVTLayer } from "@deck.gl/geo-layers";
-import geolib from "@gisatcz/deckgl-geolib";
+import * as dat from 'dat.gui';
+import {SimpleMeshLayer} from "@deck.gl/mesh-layers";
 import chroma from "chroma-js";
-import { scaleLinear } from 'd3-scale';
+import geolib from "@gisatcz/deckgl-geolib";
 import { PointCloudLayer } from '@deck.gl/layers';
 import {_TerrainExtension as TerrainExtension, DataFilterExtension} from '@deck.gl/extensions';
-import {SimpleMeshLayer} from "@deck.gl/mesh-layers";
-import {OBJLoader} from "@loaders.gl/obj";
 
 const CogTerrainLayer = geolib.CogTerrainLayer;
-
-const colorScale = chroma
-    .scale(['#b1001d', '#ca2d2f', '#e25b40', '#ffaa00', '#ffff00', '#a0f000', '#4ce600', '#50d48e', '#00c3ff', '#0f80d1', '#004ca8', '#003e8a'])
-    .domain([-5, 5]);
-
-const sphereSizeScale = scaleLinear([0.45, 1], [2, 9]).clamp(true);
-
 
 const INITIAL_VIEW_STATE = {
     longitude: 14.015511800867504,
@@ -29,137 +24,196 @@ const INITIAL_VIEW_STATE = {
     bearing: 0,
 };
 
-const scaleXYArrowWidth = scaleLinear([0.1, 20], [0.1 , 0.3*1000]).clamp(true);
-const scaleZArrowLength = scaleLinear([0.1, 20], [0.05, 2*1000]).clamp(true);
+const colorScale = chroma
+    .scale(['#b1001d', '#ca2d2f', '#e25b40', '#ffaa00', '#ffff00', '#a0f000', '#4ce600', '#50d48e', '#00c3ff', '#0f80d1', '#004ca8', '#003e8a'])
+    .domain([-5, 5]);
 
-const ARROW_SIZE = 67; // eyeball measured, only for this object: https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/arrow_v3.obj
+// const sphereSizeScale = scaleLinear([0.45, 1], [2, 9]).clamp(true);
+// const scaleXYArrowWidth = scaleLinear([0.1, 20], [0.1 , 0.3*1000]).clamp(true);
+// const scaleZArrowLength = scaleLinear([0.1, 20], [0.05, 2*1000]).clamp(true);
+//
+// const ARROW_SIZE = 67; // eyeball measured, only for this object: https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/arrow_v3.obj
 
 const insarPointRelLenThresholdInterval = [0.45, 1000000];
 
-const meshArrow = new SimpleMeshLayer(
-    {
-        id: 'insar-points-arrow',
-        data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_DESC_upd3_psd_los_4326_height_mesh_v2.json',
-        mesh: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/test_arrow.obj',
-        getColor: (d) => [...colorScale(d.properties.vel_rel).rgb(), 255],
-        getOrientation: (d) => [0,d.properties.az_ang,90],
-        getPosition: (d) => [d.geometry.coordinates[0], d.geometry.coordinates[1], d.properties.h_dt],
-        // getSize: (d) => iconScale(Math.abs(d.properties.vel_rel))*10000,
-        getScale: (d) => {
-            return [meshIconScale(Math.abs(d.properties.vel_rel)), meshIconScale(Math.abs(d.properties.vel_rel)), meshIconScale(Math.abs(d.properties.vel_rel))]
-            // if (d.properties.vel_rel > 0) {
-            //     return [scaleXYArrowWidth(d.properties.vel_rel), scaleXYArrowWidth(d.properties.vel_rel), scaleZArrowLength(d.properties.vel_rel)];
-            // }
-            // return [scaleXYArrowWidth(Math.abs(d.properties.vel_rel)), scaleXYArrowWidth(Math.abs(d.properties.vel_rel)), scaleZArrowLength(Math.abs(d.properties.vel_rel))];
-        },
-        loaders: [OBJLoader],
-        getFilterValue: d => d.properties.rel_len,
-        filterRange: insarPointRelLenThresholdInterval,
-        extensions: [new DataFilterExtension({filterSize: 1})],
-        // pickable: true,
-        // extensions: [new TerrainExtension()],
-})
-
-const DEM = new CogTerrainLayer({
-    id: 'CogTerrainLayerD8Dem',
-    elevationData:  'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/LITC52_53_4g_5m_4326_cog_nodata.tif',
-    minZoom: 12,
-    maxZoom: 14,
-    opacity: 1,
-    isTiled: true,
-    useChannel: null,
-    tileSize: 256,
-    meshMaxError: 1,
-    operation: 'terrain+draw',
-    terrainOptions: {
-        type: 'terrain',
-        multiplier: 1,
-        terrainSkirtHeight: 1,
-    }
-})
-const iconScale = scaleLinear([0,1], [10,50]).clamp(true)
+const iconScale = scaleLinear([0,5], [10,18]).clamp(true)
 const meshIconScale= scaleLinear([0,1], [10,200]).clamp(true)
 
-const iconArrow = new IconLayer({
-    id: 'icon-arrow',
+const arrowIconOptions = {
     data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_DESC_upd3_psd_los_4326_selected_arrows.geojson',
     getColor: (d) => [...colorScale(d.properties.vel_rel).rgb(), 255],
-    getIcon: (d) => d.properties.vel_rel > 0 ? 'arrow': 'arrow-filled',
+    // getIcon: (d) => d.properties.vel_rel > 0 ? 'right-arrow': 'right-filled-arrow',
+    getIcon: (d) =>'right-arrow',
     getPosition: (d) => [...d.geometry.coordinates, 1],
-    getAngle: (d) => d.properties.az_ang,
+    getAngle: (d) => 90 - (180 + d.properties.az_ang),
     billboard: false,
     sizeUnits: "meters",
     getSize: (d) => iconScale(Math.abs(d.properties.vel_rel)),
-   // mask: true,
-    iconAtlas: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/arrow_icon_atlas.png',
-    iconMapping: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/arrow_icon_atlas.json',
-    getFilterValue: d => Math.abs(d.properties.coh),
-    filterRange: [0.4, 40],
-    extensions: [new DataFilterExtension({filterSize: 1})],
-    // extensions: [new TerrainExtension()],
-});
+    // mask: true,
+    iconAtlas: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/right-arrow-atlas.png',
+    iconMapping: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/right-arrow-atlas.json',
+    getFilterValue: d => (d.properties.rel_len),
+    filterRange: insarPointRelLenThresholdInterval,
+    extensions: [new DataFilterExtension({filterSize: 1}), new TerrainExtension()],
+    // extensions: [],
+    visible: false,
+}
 
-const mvt_insar_points = new MVTLayer({
-    // data ascending
-    // data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_ASC_upd3_psd_los_4326_height/{z}/{x}/{y}.pbf',
-    // data descending
-    data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_DESC_upd3_psd_los_4326_height/{z}/{x}/{y}.pbf',
-    binary: false,
-    renderSubLayers: (props) => {
-        if (props.data) {
-            return new PointCloudLayer({
-                ...props,
-                id: `${props.id}-sphere`,
-                pickable: false,
-                sizeUnits: 'meters',
-                pointSize: 7,
-                // dve varianty vysek 'h' a 'h_dtm' podle H.Kolomaznika se to musi otestovat co bude vypadat lepe
-                // getPosition: (d) => [...d.geometry.coordinates, d.properties.h],
-                // getPosition: (d) => [...d.geometry.coordinates, d.properties.h_dtm + 5],
-                getPosition: (d) => [...d.geometry.coordinates],
-                getColor: (d) => [...colorScale(d.properties.vel_rel).rgb(), 255],
-                getFilterValue: d => Math.abs(d.properties.coh),
-                filterRange: [0.4, 40],
-                extensions: [new DataFilterExtension({filterSize: 1})],
+// Layer configurations
+const layerConfigs = [
+    {
+        id: 'tile-layer',
+        type: TileLayer,
+        options: {
+            visible:true,
+            data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            minZoom: 0,
+            maxZoom: 19,
+            tileSize: 256,
+            renderSubLayers: (props) => {
+                const { bbox: { west, south, east, north } } = props.tile;
+                return new BitmapLayer(props, {
+                    data: null,
+                    image: props.data,
+                    bounds: [west, south, east, north],
+                });
+            },
+        },
+        name: 'Basemap',
+    },
+    {
+        id: 'insar-points-arrow-mesh',
+        type: SimpleMeshLayer,
+        options: {
+            data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_DESC_upd3_psd_los_4326_height_mesh_v2.json',
+            mesh: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/arrow_filled.obj',
+            id: 'insar-points-arrow-mesh',
+            getColor: (d) => [...colorScale(d.properties.vel_rel).rgb(), 255],
+            // [0,azimuth (horizontalni) - aby sipka ukazovala nahoru, hodnota je 90, hodnota 90 - aby sipka byla placata]
+            getOrientation: (d) => [-20,90 - (180 + d.properties.az_ang),90],
+            getPosition: (d) => [d.geometry.coordinates[0], d.geometry.coordinates[1], d.properties.h_dtm + 2],
+            getScale: (d) => {
+                return [meshIconScale(Math.abs(d.properties.vel_rel)), meshIconScale(Math.abs(d.properties.vel_rel)), meshIconScale(Math.abs(d.properties.vel_rel))]
+                // if (d.properties.vel_rel > 0) {
+                //     return [scaleXYArrowWidth(d.properties.vel_rel), scaleXYArrowWidth(d.properties.vel_rel), scaleZArrowLength(d.properties.vel_rel)];
+                // }
+                // return [scaleXYArrowWidth(Math.abs(d.properties.vel_rel)), scaleXYArrowWidth(Math.abs(d.properties.vel_rel)), scaleZArrowLength(Math.abs(d.properties.vel_rel))];
+            },
+            loaders: [OBJLoader],
+            getFilterValue: d => d.properties.rel_len,
+            filterRange: insarPointRelLenThresholdInterval,
+            // pickable: true,
+            extensions: [new DataFilterExtension({filterSize: 1})],
+            visible: true,
+        },
+        name: 'arrows mesh',
+    },
+    {
+        id: 'insar-points-arrow-icon',
+        type: IconLayer,
+        options: {...arrowIconOptions},
+        name: 'arrows icon',
+    },
+    {
+        id: 'insar-points-arrow-icon-mvt',
+        type: MVTLayer,
+        options: {
+            data: 'https://gisat-gis.eu-central-1.linodeobjects.com/3dflus/d8/InSAR/trim_d8_DESC_upd3_psd_los_4326_height_v2/{z}/{x}/{y}.pbf',
+                binary: false,
+                renderSubLayers: (props) => {
+                    if (props.data) {
+                        return new IconLayer({
+                            ...arrowIconOptions,
+                            ...props,
+                            id: `${props.id}-icon`,
+                        });
+                    }
+                    return null;
+                },
+                visible: false,
+                minZoom: 8,
+                maxZoom: 14,
+        },
+        name: 'arrows icon MVT',
+    },
+    {
+        id: 'CogTerrainLayerCUZK',
+        type: CogTerrainLayer,
+        options: {
+            elevationData:  'https://gisat-data.eu-central-1.linodeobjects.com/3DFlus_GST-22/app-esa3DFlusPilot3Geology/dev/rasters/EL-GRID_COGeoN.tif',
+            minZoom: 12,
+            maxZoom: 14,
+            opacity: 1,
+            isTiled: true,
+            useChannel: null,
+            tileSize: 256,
+            meshMaxError: 1,
+            visible: true,
+            operation: 'terrain+draw',
+            terrainOptions: {
+                type: 'terrain',
+                multiplier: 1,
+                terrainSkirtHeight: 1,
+            }
+        },
+        name: 'DTM CUZK 5m',
+    },
+];
+
+// Function to create a layer based on its configuration, visibility, and properties
+const createLayer = (config, visibility, properties) => {
+    const options = { ...config.options, id: config.id, visible: visibility };
+    if (properties) {
+        Object.keys(properties).forEach((key) => {
+            options[key] = properties[key];
+        });
+    }
+    return new config.type(options);
+};
+
+function MapApp1() {
+    // Initial visibility state and layer properties for all layers
+    const initialVisibility = layerConfigs.reduce((acc, config) => {
+        acc[config.id] = config.options.visible;
+        return acc;
+    }, {});
+
+    const initialProperties = layerConfigs.reduce((acc, config) => {
+        if (config.controls) {
+            acc[config.id] = {};
+            Object.keys(config.controls).forEach(key => {
+                acc[config.id][key] = config.options[key];
             });
         }
-        return null;
-    },
-    minZoom: 8,
-    maxZoom: 14,
-})
+        return acc;
+    }, {});
 
-const osm_basemap = new TileLayer({
-    data: 'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    id: 'standard-tile-layer',
-    minZoom: 0,
-    maxZoom: 19,
-    tileSize: 256,
+    const [layerVisibility, setLayerVisibility] = useState(initialVisibility);
+    const [layerProperties, setLayerProperties] = useState(initialProperties);
 
-    renderSubLayers: (props) => {
-        const {
-            bbox: {
-                west, south, east, north,
-            },
-        } = props.tile;
+    // Update the layers array based on visibility and properties state
+    const layers = layerConfigs.map(config => createLayer(config, layerVisibility[config.id], layerProperties[config.id]));
 
-        return new BitmapLayer(props, {
-            data: null,
-            image: props.data,
-            bounds: [west, south, east, north],
+    useEffect(() => {
+        // Initialize dat.gui
+        const gui = new dat.GUI();
+
+        // Add visibility controls for each layer
+        layerConfigs.forEach(config => {
+            gui.add(layerVisibility, config.id).name(config.name).onChange((value) => {
+                setLayerVisibility((prevState) => ({
+                    ...prevState,
+                    [config.id]: value,
+                }));
+            });
         });
-    },
-    // extensions: [new TerrainExtension()],
-})
 
-const layers = [
-    DEM,
-    // mvt_insar_points,
-    meshArrow,
-    // iconArrow,
-    osm_basemap
-]
-function MapApp1() {
+        // Cleanup dat.gui on unmount
+        return () => {
+            gui.destroy();
+        };
+    }, []);
+
     return (
         <DeckGL
             initialViewState={INITIAL_VIEW_STATE}
