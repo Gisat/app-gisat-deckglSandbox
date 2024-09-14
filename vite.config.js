4f18b6a2fc70a33a69594a7461dc7abacd679fa4
Mariana Kecová <mariana.kecova@gisat.cz>
2024-09-14 20:34:43 +0200

    Update config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/./', // Replace with your repository name
+  base: '/app-gisat-deckglSandbox/'
+  // base: '/./', // Replace with your repository name
 })
