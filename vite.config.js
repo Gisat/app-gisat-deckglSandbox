364211624b79ab36903790fb3cd00706b2b76a51
Mariana Kecova <mariana.kecova@gisat.cz>
2024-09-18 14:40:53 +0200

    Vite config base updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/app-gisat-deckglSandbox/'
-  base: process.env.NODE_ENV === 'production' ? '/app/' : '/', // Use '/app/' for production, '/' for development
+  base: process.env.NODE_ENV === 'production' ? '/app-gisat-deckglSandbox/' : '/', // Use '/app/' for production, '/' for development
 })
