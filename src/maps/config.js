f7faf8610018fdbd325cbf92222f9f43c0e6aff5
Mariana Kecova <mariana.kecova@gisat.cz>
2024-11-08 21:12:19 +0100

    Update layer name

--- src/maps/config.js
+++ src/maps/config.js
@@ -13,7 +13,7 @@ const mapApps = [
     { name: 'P1 Metro D: Google 3D Tiles', path: '/metro-d-google-3d-tiles', component: MetroD_Google3DTiles },
     { name: 'P3 D8: points as spheres', path: '/d8-mvt-point-cloud-spheres', component: MVTPointCloudSpheres },
     { name: 'P3 D8: 2D arrows', path: '/d8-icon-arrows', component: D8_iconArrows },
-    { name: 'P3 D8: data filter extension', path: '/d8-filter', component: D8_filter },
+    { name: 'P3 D8: 3D arrows', path: '/d8-filter', component: D8_filter },
     { name: 'P3 D8: vertical profile', path: '/vertical_profile', component: vertical_profile },
     // { name: 'Google 3D Tiles', path: '/google-3d-tiles', component: Google3DTiles },
     // { name: 'Test App', path: '/test-app', component: MapApp1 },
